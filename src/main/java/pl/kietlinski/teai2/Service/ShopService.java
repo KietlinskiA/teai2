636f589bd216a0a610ad8e9fa62409b1e312ddb4
KietlinskiA <adrian.kietlinski@interia.pl>
2021-06-07 13:06:37 +0200

    Add logger

--- src/main/java/pl/kietlinski/teai2/Service/ShopService.java
+++ src/main/java/pl/kietlinski/teai2/Service/ShopService.java
@@ -1,20 +1,27 @@
 package pl.kietlinski.teai2.Service;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import pl.kietlinski.teai2.Model.Product;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @Service
 public class ShopService {
 
+    private Logger logger;
     private List<Product> productList;
     private int sumPrices;
 
     public ShopService() {
+        logger = LoggerFactory.getLogger(ShopService.class);
         productList = new ArrayList<>();
         init();
         getSumPricesValue();
@@ -31,6 +38,11 @@ public class ShopService {
         productList.add(product3);
         productList.add(product4);
         productList.add(product5);
+        if (productList.isEmpty()) {
+            logger.error("Prepare data was not created");
+        } else {
+            logger.info("Prepare data was created");
+        }
     }
 
     protected void getProductListForeach() {
@@ -41,6 +53,7 @@ public class ShopService {
         for(Product product : productList){
             sumPrices += product.getPrice();
         }
+        logger.info("Sum of prices was counted");
     }
 
 }
